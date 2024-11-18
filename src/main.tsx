dfce630734f292ca5d8e4b2679d2a245c1c90834
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-11-19 01:24:01 +0900

    add:apolloプロバイダーの追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apolloClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
 )
