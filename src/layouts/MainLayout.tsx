7a3d5656a99dcc10ed48ad47f683e304a729d33f
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-11-26 00:24:57 +0900

    add:mantineのスタイリング確認

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,8 +2,10 @@ import { Flex } from '@mantine/core'
 
 const MainLayout = ({ children }: { children: React.ReactElement }) => {
   return (
-    <Flex>
-      <Flex>{children}</Flex>
+    <Flex align={'center'} justify={'center'} h={'100vh'}>
+      <Flex bg="gray" align={'center'} justify="center">
+        {children}
+      </Flex>
     </Flex>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import './index.css'
+import '@mantine/core/styles.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import { ApolloProvider } from '@apollo/client'
@@ -9,33 +8,33 @@ import { client } from './apolloClient'
 import Home from './pages/Home.tsx'
 import { MantineProvider } from '@mantine/core'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Home />,
-      children: [
-        {
-          path: '/chatrooms/:id'
-        }
-      ]
-    }
-  ],
+const router = createBrowserRouter([
   {
-    future: {
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true
-    }
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/chatrooms/:id'
+      }
+    ]
   }
-)
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <MantineProvider>
-        <RouterProvider router={router} />
+        <RouterProvider
+          router={router}
+          future={{
+            v7_fetcherPersist: true,
+            v7_normalizeFormMethod: true,
+            v7_partialHydration: true,
+            v7_relativeSplatPath: true,
+            v7_skipActionErrorRevalidation: true,
+            v7_startTransition: true
+          }}
+        />
       </MantineProvider>
     </ApolloProvider>
   </React.StrictMode>
