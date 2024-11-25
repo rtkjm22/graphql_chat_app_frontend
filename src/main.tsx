451cabb8f3ddf2480b1b2eac0bca0ce3baa7a94d
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-11-25 23:34:24 +0900

    add:main.tsの初期化

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './apolloClient.ts'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apolloClient'
+import Home from './pages/Home.tsx'
+import { MantineProvider } from '@mantine/core'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+      children: [
+        {
+          path: '/chatrooms/:id'
+        }
+      ]
+    }
+  ],
+  {
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true
+    }
+  }
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </ApolloProvider>
-  </StrictMode>
+  </React.StrictMode>
 )
