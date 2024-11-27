41e039c5e6598f1f8a42cedfe41ac5c5ec1b154b
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2024-11-27 23:20:34 +0900

    fix:リセットCSSの追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import '@mantine/core/styles.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import { ApolloProvider } from '@apollo/client'
